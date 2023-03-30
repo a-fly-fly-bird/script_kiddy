4fc05203b411c5fc723e2b49ba8406dd6766d9af
Lucas Tan <lucas.y.tan.life@outlook.com>
2023-03-30 14:40:16 +0800

    update

---
+++ run/yolo_slowfast/run.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/wufan-tb/yolo_slowfast
+cd yolo_slowfast
+conda create -n yolo-slownfast python=3.8 -y
+conda activate yolo-slownfast
+echo "pytorchvideo\nscipy\npandas\nrequests\npsutil\nmatplotlib\nseaborn\nnumpy<=1.23" > requirements.txt
+pip install -r requirements.txt
+# 传文件进容器用docker cp, 或者还是应该用目录映射。这种不是基础的文件不要写入dockerfile。
